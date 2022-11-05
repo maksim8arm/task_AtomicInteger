2c05da9d5978d8b521a405e78e5af2002af32063
Makism.Armenskiy <maxim.arm8@mail.ru>
2022-11-05 20:53:40 +0300

    fixed mistakes in task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
     public static int threeLetter = 0;
     public static int fourLetter = 0;
-    public static int letters = 0;
+    public static int fiveLetter = 0;
 
     public static void main(String[] args) throws InterruptedException {
 
         AtomicInteger threeLetterAtom = new AtomicInteger(threeLetter);
         AtomicInteger fourLetterAtom = new AtomicInteger(fourLetter);
-        AtomicInteger lettersAtom = new AtomicInteger(letters);
+        AtomicInteger fiveLetterAtom = new AtomicInteger(fiveLetter);
 
         Random random = new Random();
         String[] texts = new String[100_000];
@@ -20,25 +20,51 @@ public class Main {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
 
-        Runnable fourLettterMethod = () -> {
+        Runnable palidromMethod = () -> {
             for (String str : texts) {
-                if (str.equals("abba")) {
-                    fourLetterAtom.incrementAndGet();
+                int length = str.length();
+                int j = 0;
+                int forward = 0;
+                int backward = length - 1;
+                while (backward > forward) {
+                    char forwardChar = str.charAt(forward++);
+                    char backwardChar = str.charAt(backward--);
+                    if (forwardChar != backwardChar) {
+                        j = 0;
+                        break;
+                    }
+                    j = 1;
+                }
+                if (j == 1) {
+                    if (str.length() == 3) threeLetterAtom.incrementAndGet();
+                    else if (str.length() == 4) fourLetterAtom.incrementAndGet();
+                    else if (str.length() == 5) fiveLetterAtom.incrementAndGet();
                 }
             }
-            System.out.println("Красивых слов \"abba\" с длиной 4: " + fourLetterAtom.get());
         };
 
-        Runnable threeLetterMethod = () -> {
+        Runnable sameLetterMethod = () -> {
             for (String str : texts) {
-                if (str.equals("aaa")) {
-                    threeLetterAtom.incrementAndGet();
+                int i = 0;
+                int j = 0;
+                while (i < str.length() - 1) {
+                    if (str.charAt(i) == str.charAt(i + 1)) {
+                        j = 1;
+                        i++;
+                    } else {
+                        j = 0;
+                        break;
+                    }
+                }
+                if (j == 1) {
+                    if (str.length() == 3) threeLetterAtom.incrementAndGet();
+                    else if (str.length() == 4) fourLetterAtom.incrementAndGet();
+                    else if (str.length() == 5) fiveLetterAtom.incrementAndGet();
                 }
             }
-            System.out.println("Красивых слов \"aaa\" с длиной 3: " + threeLetterAtom.get());
         };
 
-        Runnable lettersMethod = () -> {
+        Runnable upLetterMethod = () -> {
             for (String str : texts) {
                 int i = 0;
                 int j = 0;
@@ -51,33 +77,36 @@ public class Main {
                     }
                     i++;
                 }
-
                 if (j == 1) {
-                    lettersAtom.incrementAndGet();
+                    if (str.length() == 3) threeLetterAtom.incrementAndGet();
+                    else if (str.length() == 4) fourLetterAtom.incrementAndGet();
+                    else if (str.length() == 5) fiveLetterAtom.incrementAndGet();
                 }
-
             }
-            System.out.println("Красивых слов с буквами по порядку: " + lettersAtom.get());
         };
 
-        Thread threadThree = new Thread(threeLetterMethod);
-        Thread threadFour = new Thread(fourLettterMethod);
-        Thread threadLetters = new Thread(lettersMethod);
+        Thread threadSameLetter = new Thread(sameLetterMethod);
+        Thread threadPalidrom = new Thread(palidromMethod);
+        Thread threadUpLetter = new Thread(upLetterMethod);
 
-        threadThree.start();
-        threadFour.start();
-        threadLetters.start();
+        threadSameLetter.start();
+        threadPalidrom.start();
+        threadUpLetter.start();
 
-        threadThree.join();
-        threadFour.join();
-        threadLetters.join();
+        threadSameLetter.join();
+        threadPalidrom.join();
+        threadUpLetter.join();
+
+        System.out.println("Красивых слов с длиной 3: " + threeLetterAtom.get());
+        System.out.println("Красивых слов с длиной 4: " + fourLetterAtom.get());
+        System.out.println("Красивых слов с длиной 5: " + fiveLetterAtom.get());
     }
 
-    public static String generateText(String letters, int length) {
+    public static String generateText(String fiveLetter, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
+            text.append(fiveLetter.charAt(random.nextInt(fiveLetter.length())));
         }
         return text.toString();
     }
